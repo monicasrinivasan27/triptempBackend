f46c5f656cbb8544fa11f2e314288c8a4be02200
monicasrinivasan27 <srinivasan.monica@gmail.com>
2024-05-02 19:06:15 -0500

    Created a spring boot backend

---
+++ src/main/java/com/temperature/triptempBackend/TriptempBackendApplication.java
@@ -0,0 +1,13 @@
+package com.temperature.triptempBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptempBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptempBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temperature/triptempBackend/TriptempBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temperature.triptempBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptempBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
