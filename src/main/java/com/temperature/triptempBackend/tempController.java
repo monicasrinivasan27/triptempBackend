21ffda2746b05ebe41045d82f38579193a35e3cb
monicasrinivasan27 <srinivasan.monica@gmail.com>
2024-05-25 20:56:13 -0500

    Included

--- src/main/java/com/temperature/triptempBackend/tempController.java
+++ src/main/java/com/temperature/triptempBackend/tempController.java
@@ -1,4 +1,12 @@
 package com.temperature.triptempBackend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/temp")
+
 public class tempController {
 }
